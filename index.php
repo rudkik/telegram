c4ca1e5a5e1309a0715102c818ec53844e468ccf
rudkik <66820365+rudkik@users.noreply.github.com>
2021-11-01 12:49:52 +0300

    2

--- index.php
+++ index.php
@@ -1,5 +1,30 @@
 <?php
- $data = json_decode(file_get_contents('php://input'), TRUE);
- file_put_contents( 'file.txt', '$data: '. print_r($data,1). "\n", FILE_APPEND);
 
- 
+define( 'TOKEN', '1080770740:AAFP-1wHRynILUoqv2Jmge1xMwCPC7DfdcU');
+
+$data = json_decode(file_get_contents('php://input'), TRUE);
+file_put_contents( 'file.txt', '$data: '. print_r($data,1). "\n", FILE_APPEND);
+
+
+
+
+$message = mb_strtolower($data['message']['text'], 'utf-8');
+
+switch ($message) {
+    case '123':
+        $method = 'sendMessage';
+        $params = [
+            'chat_id' => $data['message']['chat']['id'],
+            'text' => 'hello'
+        ];
+        break;
+    default:
+        $method = 'sendMessage';
+        $params = [
+            'chat_id' => $data['message']['chat']['id'],
+            'text' => 'ошибка'
+        ];
+}
+file_get_contents('https://api.telegram.org/bot'. TOKEN . '/' . $method . '?' . http_build_query($params));
+
+
